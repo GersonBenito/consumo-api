31309e606f6e05f7ef71bbe62236ffa57fdee463
GersonBenito <gersonbenito4@gmail.com>
2022-02-27 12:31:52 -0600

    feat(varios) se agrego responsive a las cards de los productos y se corrigio tamano de imagenes, titulo y descripcion

--- src/services/product/product.service.ts
+++ src/services/product/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
 import { Product } from 'src/interfaces/product.interface';
 
@@ -10,6 +10,9 @@ import { Product } from 'src/interfaces/product.interface';
 export class ProductService {
 
   private url: string = env.urlBase;
+  
+  public dataProduct = new BehaviorSubject<number>(0);
+  
 
   constructor( private http: HttpClient ) { }
 
@@ -17,6 +20,10 @@ export class ProductService {
 
     return this.http.get<Product[]>(this.url);
     
+  }
+
+  getProductById(id: number): Observable<Product>{
+    return this.http.get<Product>(`${this.url}/${id}`);
   }
 
 }
